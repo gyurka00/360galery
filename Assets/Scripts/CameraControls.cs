cdc1eb66526274ef31aac70f9b330dd54ba6cc40
György Csadó <gyorgy.csado@codeandsoda.com>
2022-10-28 09:55:28 +0200

    initial commit

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    public float speed = 10;
+
+    void Update()
+    {
+
+        if (Time.deltaTime > 0.2f) return; // Prevent Random bullshit rotation on app loading or lag
+
+        // LOOK
+
+        transform.Rotate(new Vector3( // look up / down
+            -Input.GetAxis("Mouse Y"),
+            0,
+            0
+        ), Space.Self);
+
+        transform.Rotate(new Vector3( // look left / right
+            0,
+            Input.GetAxis("Mouse X"),
+            0
+        ), Space.World); // Space.World to avoid ROLL: https://en.wikipedia.org/wiki/Aircraft_principal_axes#/media/File:Yaw_Axis_Corrected.svg
+
+        // MOVE (forward, back, left, right)
+
+        transform.position += transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.position += transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+
+        transform.Rotate(new Vector3( // look up / down
+            -Input.GetAxisRaw("Vertical"),
+            0,
+            0
+        ), Space.Self);
+
+        transform.Rotate(new Vector3( // look left / right
+            0,
+            Input.GetAxisRaw("Horizontal"),
+            0
+        ), Space.World);
+        // MOVE (up, down)
+
+        if (Input.GetButton("Fire1"))
+        {
+            transform.position += transform.up * Time.deltaTime * speed * 0.6f;
+        }
+        if (Input.GetButton("Fire2"))
+        {
+            transform.position -= transform.up * Time.deltaTime * speed * 0.6f;
+        }
+    }
+}
